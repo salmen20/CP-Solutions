718dee315c770ad60a88676eb55a75f6175e6321
salmen20 <62488705+salmen20@users.noreply.github.com>
2022-09-21 15:09:30 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1030A | In Search of an Easy Problem/133799607.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+ 
+int t;
+ cin>>t;
+ int a;
+ while(t--){
+        cin>>a;
+        if (a==1){
+            cout<<"HARD"<<endl;
+            return 0;
+        }
+        
+ 
+ 
+ }
+ cout<<"EASY"<<endl;
+ 
+ 
+}
