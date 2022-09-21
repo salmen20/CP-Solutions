97256ab030da3be0e229b92e47d60cbbb57bcef6
salmen20 <62488705+salmen20@users.noreply.github.com>
2022-09-21 15:08:58 +0100

    Committing files

---
+++ CodeForces/GNU C++17/659A | Round House/144456024.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl ;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int)(x).size())
+#define max(a,b) (a>b ? a : b)
+#define min(a,b) (a<b ? a : b)
+#define fastio ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
+#define endl '\n'
+ 
+const int MOD = (1e9)+7 ;
+const int N = 2e5 + 5;
+const double PI = acos(-1);
+const double eps = 1e-10;
+const long long oo = 1e10;
+const int K = 26;
+const int M=(1e9)+7;
+const ll inf = (1LL << 61) - 1;
+const int INF =1e6+2;
+void yes()
+{
+    cout<<"YES\n";
+}
+void no()
+{
+    cout<<"NO\n";
+}
+ 
+ 
+int main(){
+  int n,a,b;
+  cin>>n>>a>>b;
+  int sum=0;
+  b%=n;
+  if(b<0){
+    sum=n+b+a;
+  }
+  else {
+    sum=a+b;
+  }
+  if(sum%n==0){
+    cout<<n<<endl;
+  }
+  else {
+    cout<<sum%n<<endl;
+  }
+ 
+ 
+return 0;
+}
